d6005299d45ceb789c75f5e0a0ded4341bdc7ae9
Siobhan <siobhanbrown559@gmail.com>
2021-10-08 14:47:29 +0100

    user validations added, destroy method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,44 @@
 class Api::V1::UsersController < Api::V1::BaseController
+  before_action :set_user,  only: [ :show, :update ]
+
   def index
     @users = policy_scope(User)
     render json: @users
   end
 
   def show
+    render json: @user
+  end
+
+  def update
+    if @user.update(user_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
+  def destroy
+    if authorization == bearer[API_KEY]
+      @user.destroy
+    else
+      render_error
+    end
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
     authorize @user
-    render json: @user
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :username)
+  end
+
+  def render_error
+    render json: { errors: @user.errors.full_messages },
+      status: :unprocessable_entity
   end
 end
--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -8,4 +8,9 @@ class UserPolicy < ApplicationPolicy
   def show?
     true
   end
+
+  def update?
+    record.user == user
+  end
+
 end
